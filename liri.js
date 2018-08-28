15a7e8f25a777378cdf58b76f73627b894ab9954
Sajeel Malik <sajeel23@berkeley.edu>
2018-08-27 23:09:04 -0700

    improve spotify API search functionality

--- liri.js
+++ liri.js
@@ -1,4 +1,15 @@
+//Sajeel Malik
+//LIRI BOT
+
+
+//require all .env files
 require("dotenv").config();
+
+//import keys.js and node-spotify-api so that we can access the spotify export
+var keys = require("./keys.js");
+var Spotify = require('node-spotify-api');
+
+var spotify = new Spotify(keys.spotify);
 
 var request = require("request");
 var fs = require("fs");
@@ -6,19 +17,26 @@ var fs = require("fs");
 var command = process.argv[2];
 var searchTerm = process.argv[3];
 
-switch (command){
-  case "concert-this":
-    songLocation();
-    break;
-  case "spotify-this-song":
-    spotifySearch(searchTerm);
-    break;
-  case "movie-this":
-    movieInfo();
-    break;
-  case "do-what-it-says":
-    break;
+function game(){
+    switch (command){
+      case "concert-this":
+        songLocation();
+        break;
+      case "spotify-this-song":
+        spotifySearch();
+        break;
+      case "movie-this":
+        movieInfo();
+        break;
+      case "do-what-it-says":
+        doRandom();
+        break;
+    }
+
 }
+
+game();
+
 
 function songLocation(){
 
@@ -29,40 +47,52 @@ function songLocation(){
 
     // If the request is successful
     if (!error && response.statusCode === 200) {
-  
-      // Parse the body of the site and recover just the imdbRating
-      // (Note: The syntax below for parsing isn't obvious. Just spend a few moments dissecting it).
-      console.log("Venue Name: " + JSON.parse(body).venue.name);
-      console.log("Venue Location: " + JSON.parse(body).venue.city);
-      console.log("Date of Show: " + JSON.parse(body).datetime);
+
+      //show just the first result
+      // console.log(JSON.parse(body)[0]);
+
+      console.log("Venue Name: " + JSON.parse(body)[0].venue.name);
+      console.log("Venue Location: " + JSON.parse(body)[0].venue.city);
+      console.log("Date of Show: " + JSON.parse(body)[0].datetime);
     }
   });
   
 }
 
-function spotifySearch(song){
+function spotifySearch(){
 
-  var queryUrl = "http://www.omdbapi.com/?t=" + movieName + "&y=&plot=short&apikey=trilogy";
+  if(searchTerm){
 
-  request(queryUrl, function(error, response, body) {
+    spotify.search({ type: 'track', query: searchTerm }, function(err, data) {
+      if (err) {
+        return console.log('Error occurred: ' + err);
+      }
+   
+      // console.log(data.tracks.items[0]); 
 
-    // If the request is successful
-    if (!error && response.statusCode === 200) {
+      console.log("Artist: " + data.tracks.items[0].artists[0].name);
+      console.log("Song Name: " + data.tracks.items[0].name);
+      console.log("Spotify Preview Link: " + data.tracks.items[0].external_urls.spotify);
+      console.log("Album: " + data.tracks.items[0].album.name);
+    });
+  }
+  else{
+    spotify.search({ type: 'track', query: "The Sign by Ace of Base" }, function(err, data) {
+      if (err) {
+        return console.log('Error occurred: ' + err);
+      }
+     
+      console.log(data.tracks.items[0]); 
+      });
+  }
   
-      // Parse the body of the site and recover just the imdbRating
-      // (Note: The syntax below for parsing isn't obvious. Just spend a few moments dissecting it).
-      console.log("Movie Title: " + JSON.parse(body).Title);
-      console.log("Release Year: " + JSON.parse(body).Year);
-      console.log("IMDB Rating: " + JSON.parse(body).Rating);
-    }
-  });
   
 }
 
 function movieInfo(){
 
   var movie = searchTerm;
-  var queryUrl = "http://www.omdbapi.com/?t=" + movieName + "&y=&plot=short&apikey=trilogy";
+  var queryUrl = "http://www.omdbapi.com/?t=" + movie + "&y=&plot=short&apikey=trilogy";
 
   request(queryUrl, function(error, response, body) {
 
@@ -73,9 +103,15 @@ function movieInfo(){
       // (Note: The syntax below for parsing isn't obvious. Just spend a few moments dissecting it).
       console.log("Movie Title: " + JSON.parse(body).Title);
       console.log("Release Year: " + JSON.parse(body).Year);
-      console.log("IMDB Rating: " + JSON.parse(body).Rating);
+      console.log("IMDB Rating: " + JSON.parse(body).imdbRating);
+      console.log("Rotten Tomatoes Rating: " + JSON.parse(body).Ratings[1].value);
+      console.log("Country: " + JSON.parse(body).Country);
+      console.log("Language: " + JSON.parse(body).Language);
+      console.log("Plot: " + JSON.parse(body).Plot);
+      console.log("Actors: " + JSON.parse(body).Actors);
     }
   });
+  
   
 }
 
@@ -88,9 +124,13 @@ function doRandom(){
     }
   
     // We will then print the contents of data as a single string
-    console.log(data);
+    var inputs = data.split(",");
+    console.log(inputs);
   
-    spotifySearch(data);
+    command = inputs[0];
+    searchTerm = inputs[1];
+
+    game();
   
   });
 }
